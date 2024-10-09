4819b0cc0b8f94cb0c6c1355a34df93c74d39273
joaopfmonteiro <joaoosverdes@gmail.com>
2024-10-09 07:42:25 +0100

    Creation of tests for GetAll, GetById, and Post methods. Creation of the Users entity and their respective controllers. Connection with H2 database

---
+++ src/main/java/com/joao_monteiro/springboot_crud_tdd/SpringBootCrudtddApplication.java
@@ -0,0 +1,13 @@
+package com.joao_monteiro.springboot_crud_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudtddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudtddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao_monteiro/springboot_crud_tdd/SpringBootCrudtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao_monteiro.springboot_crud_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudtddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
